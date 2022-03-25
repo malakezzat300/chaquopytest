b65656ac49312e92984e13cd790fd28dc645802f
malak ezzat <malak3821@gmail.com>
2022-03-25 18:24:21 +0200

    third

--- app/src/main/java/com/malakezzat/android/chaquopytest2/MainActivity.java
+++ app/src/main/java/com/malakezzat/android/chaquopytest2/MainActivity.java
@@ -15,6 +15,7 @@ import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
